00376e10dc3a5e0d30bcbf3b84ed60dc8a4c0766
kanhai <kanhai.wadekar@gmail.com>
2025-02-26 16:03:33 -0800

    New code files

--- Image_Generator/diffusion.py
+++ Image_Generator/diffusion.py
@@ -2,3 +2,10 @@ model_id = "runwayml/stable-diffusion-v1-5"
 
 from diffusers import StableDiffusionPipeline
 
+from huggingface_hub import notebook_login
+
+notebook_login()
+
+from torch import cuda 
+
+print(cuda.is_available())
